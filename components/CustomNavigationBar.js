94070b48217df21a4268574ed810db0794926499
phuocleoceo <ht10082001@gmail.com>
2022-03-24 11:16:46 +0700

    SearchBar

--- components/CustomNavigationBar.js
+++ components/CustomNavigationBar.js
@@ -1,11 +1,26 @@
-import { Appbar } from 'react-native-paper';
+import { Appbar, Searchbar } from 'react-native-paper';
+import React, { useState } from "react";
 
 export default function CustomNavigationBar({ navigation, back })
 {
+    const [searchShow, setSearchShow] = useState(false);
+    const handleSearch = () => setSearchShow(!searchShow);
+
     return (
         <Appbar.Header>
             {back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
-            <Appbar.Content title="Contact App" />
+            {
+                searchShow
+                    ?
+                    <Searchbar
+                        placeholder="Search"
+                        value=""
+                        style={{ width: "90%" }}
+                    />
+                    :
+                    <Appbar.Content title="Contact App" />
+            }
+            <Appbar.Action icon="account-search" onPress={handleSearch} />
         </Appbar.Header>
     );
 }
